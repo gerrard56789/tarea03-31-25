23edd36a08b3b91fa62427d0e46f3cd7a2208d57
Gerardo <ge6975507@gmailcom>
2025-03-31 16:20:34 -0600

    conexion

---
+++ Code-First/Models/MiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Code_First.Models
+{
+    public class MiDbContext: DbContext
+    {
+        public MiDbContext(DbContextOptions<MiDbContext> options) : base(options) { }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+
+        public DbSet<Empleado> empleados { get; set; }
+
+    }
+}
--- Code-First/Program.cs
+++ Code-First/Program.cs
@@ -1,7 +1,16 @@
+using Code_First.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 

70e6b3599ca25b1de6f308b7bf87459055de858a
Gerardo <ge6975507@gmailcom>
2025-03-31 15:37:39 -0600

    conexion de program

--- Database-First/Program.cs
+++ Database-First/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Database_First.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Practica18032025DataFirstContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
